64ff2faad063e9af37c37f0b3906d176871d97c0
sebastianselander <sebastian.selander@gmail.com>
2024-03-04 13:50:16 +0100

    Add ability for a one time sort of existing files in a directory

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Control.Concurrent (threadDelay)
-import Control.Monad (forever, void, when)
+import Control.Monad (forever, void, when, zipWithM_)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Parser (parseConfig)
@@ -12,10 +12,11 @@ import System.Directory (
     createDirectory,
     doesDirectoryExist,
     doesFileExist,
+    listDirectory,
     renameFile,
  )
 import System.Environment (getArgs)
-import System.Exit (exitFailure)
+import System.Exit (exitFailure, exitSuccess)
 import System.FSNotify (
     Event (..),
     EventIsDirectory (..),
@@ -31,9 +32,9 @@ import System.FilePath (
  )
 import System.Info (os)
 
-shouldAct :: Event -> Bool
-shouldAct Added{} = True
-shouldAct _ = False
+isFile :: Event -> Bool
+isFile (Added _ _ IsFile) = True
+isFile _ = False
 
 -- Finds non-overlappig appropriate name.
 -- Does nothing if there are no collisions
@@ -64,32 +65,32 @@ findTargetPath ::
 findTargetPath dictionary path = case takeExtension path of
     "" -> Nothing
     _ : extension ->
-        let targetDir =
-                takeDirectory path
-                    </> Map.findWithDefault
-                        extension
-                        extension
-                        dictionary
+        let ext = Map.findWithDefault extension extension dictionary
+            targetDir = takeDirectory path </> ext
             fileName = takeFileName path
-         in Just (targetDir, targetDir </> fileName)
+         in case ext of 
+            "ignore" -> Nothing
+            _        -> Just (targetDir, targetDir </> fileName)
 
 -- On a file being added to the directory,
 -- move that file to the appropriate directory
 act :: Map FilePath FilePath -> Event -> IO ()
-act dictionary (Added oldpath _time IsFile) = do
-    putStrLn ""
-    putStrLn $ "Path: " <> oldpath
-    case findTargetPath dictionary oldpath of
-        Just (targetDir, targetPath) -> do
-            doesDirectoryExist targetDir >>= \case
-                False -> do
-                    createDirectory targetDir
-                    renameFile oldpath targetPath
-                True -> do
-                    newTargetPath <- findNewTargetPath targetPath
-                    renameFile oldpath newTargetPath
-        Nothing -> return ()
+act dictionary (Added path _ IsFile) = void $ moveFile dictionary path
 act _ _ = return ()
+
+moveFile :: Map FilePath FilePath -> FilePath -> IO (Maybe String)
+moveFile dictionary path = case findTargetPath dictionary path of
+    Just (targetDir, targetPath) -> do
+        doesDirectoryExist targetDir >>= \case
+            False -> do
+                createDirectory targetDir
+                renameFile path targetPath
+                pure (pure targetPath)
+            True -> do
+                newTargetPath <- findNewTargetPath targetPath
+                renameFile path newTargetPath
+                pure (pure newTargetPath)
+    Nothing -> pure (fail "File should not be moved")
 
 directoryExist :: FilePath -> IO ()
 directoryExist path = do
@@ -99,6 +100,18 @@ directoryExist path = do
             putStrLn $ "'" <> path <> "' does not exist or is not a directory"
             exitFailure
 
+sortDirectory :: Map FilePath FilePath -> FilePath -> IO ()
+sortDirectory dict path = do
+    directoryExist path
+    files <- map (path </>) <$> listDirectory path
+    filesMoved <- mapM (moveFile dict) files
+    zipWithM_ showIfMoved files filesMoved
+  where
+    showIfMoved :: String -> Maybe String -> IO ()
+    showIfMoved from (Just to) =
+        putStrLn ("Moved '" <> from <> "' to '" <> to <> "'")
+    showIfMoved _ Nothing = pure ()
+
 main :: IO ()
 main = do
     when
@@ -106,16 +119,23 @@ main = do
         (putStrLn ("Bad operating system: " <> os) >> exitFailure)
     args <- getArgs
     (path, dictionary) <- case args of
+        ["--move", path] -> do
+            sortDirectory mempty path
+            putStrLn ("Sorted '" <> path <> "'")
+            exitSuccess
+        [path, "--move"] -> do
+            sortDirectory mempty path
+            putStrLn ("Sorted '" <> path <> "'")
+            exitSuccess
         [path] -> directoryExist path >> pure (path, mempty)
         [path, configuration] -> do
             directoryExist path
             dict <- parseConfig configuration
             pure (path, dict)
         _else -> do
-            putStrLn "Incorrect amount of arguments"
-            putStrLn "Expected:\n    file-sorter <PATH_TO_DIR> (optional: <PATH_TO_CONFIG>)"
+            putStrLn "Can't figure out what to do"
             exitFailure
     canonical <- canonicalizePath path
     mgr <- startManager
-    void $ watchDir mgr canonical shouldAct (act dictionary)
+    void $ watchDir mgr canonical isFile (act dictionary)
     void $ forever (threadDelay 1_000_000)
